2b3caba9dd7b863a69fd26af77732a55639a2dbc
rgj40q <45658038+rgj40q@users.noreply.github.com>
2019-07-08 13:23:12 +0700

    Add files via upload

--- test.sh
+++ test.sh
@@ -1,12 +1,10 @@
 #!/bin/sh
 
-./parser < test-input.txt > test-output.txt || {
+./parser < 'test-input.txt' > 'test-output.txt' || {
     echo 'test fail'
     exit
 }
-och=$(openssl dgst test-output.txt | cut -d '=' -f 2)
-ech=$(openssl dgst test-expected.txt | cut -d '=' -f 2)
-if [ "$och" = "$ech" ]; then
+if [ -z "$(diff test-output.txt test-expected.txt)" ]; then
     echo 'test ok'
 else
     echo 'test fail'
